481d9dda171466227a5d24c238ca7733b345a6b3
do thanh dat <dat@wexthuset.com>
2018-01-29 19:26:39 +0700

    socket

---
+++ appwebsocket/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+// const io = require('socket.io')(8080, {
+//   path: '/test',
+//   serveClient: false,
+//   // below are engine.IO options
+//   pingInterval: 10000,
+//   pingTimeout: 5000,
+//   cookie: false
+// });
+
+module.exports = router;
